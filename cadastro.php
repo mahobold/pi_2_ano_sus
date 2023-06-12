70c961811bdfc2586e0c5aeefc1fb6a086ed29ae
ANA BEATRIZ FERREIRA PASSOS <passos.2967@aluno.pr.senac.br>
2023-06-12 09:04:02 -0300

    Atualização Ana

--- cadastro.php
+++ cadastro.php
@@ -1,7 +1,9 @@
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
-    <link rel="stylesheet" href="starnunes.css">
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+
+    <link rel="stylesheet" href="projeto.css">
   <title>Tela de Cadastro</title>
 </head>
 <body>
@@ -15,7 +17,8 @@
       <input type="password" name="senha" placeholder="Senha" required>
       <input type="submit" value="Cadastrar">
     </form>
-    <p>É funcionario? <a href="login.php">Faça login</a></p>
+    <p> <a href="login.php">Faça login</a></p>
   </div>
 </body>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
 </html>
--- login.php
+++ login.php
@@ -1,17 +1,21 @@
 <!DOCTYPE html>
-<html>
+<html lang="pt-br">
 <head>
-    <link rel="stylesheet" href="starnunes.css">
-  <title>Página de Login</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+    <link rel="stylesheet" href="projeto.css">
+
+    <title>Página de Login</title>
 </head>
 <body>
-  <div class="login-container">
-    <h2>Login</h2>
-    <form action="login.php" method="post">
-      <input type="text" name="username" placeholder="Nome de Usuário" required>
-      <input type="password" name="password" placeholder="Senha" required>
-      <input type="submit" value="Entrar">
-    </form>
-  </div>
+    <div class="login-container">
+        <h2>Login</h2>
+        <form action="login.php" method="post">
+            <input type="text" name="username" placeholder="Nome de Usuário" required>
+            <input type="password" name="password" placeholder="Senha" required>
+            <input type="submit" value="Entrar">
+        </form>
+    </div>
 </body>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
+
 </html>
