195fbf9a4692e1157c5c07a314b4ea0cd3277b61
ANA BEATRIZ FERREIRA PASSOS <passos.2967@aluno.pr.senac.br>
2023-06-13 09:59:20 -0300

    Atualização Ana

---
+++ cadastro.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <link rel="stylesheet" href="projeto.css">
+  <title>Tela de Cadastro</title>
+</head>
+<body>
+  <div class="signup-container">
+    <h2>Cadastro</h2>
+    <form action="cadastro.php" method="post">
+      <input type="text" name="nome" placeholder="Nome Completo" required>
+      <input type="text" name="telefone" placeholder="Telefone" required>
+      <input type="text" name="endereco" placeholder="Endereço" required>
+      <input type="email" name="email" placeholder="Email" required>
+      <input type="password" name="senha" placeholder="Senha" required>
+      <input type="submit" value="Cadastrar">
+    </form>
+    <p><a href="login.php">Faça login</a></p>
+  </div>
+</body>
+</html>
+
+
+
+
+
+
+
+
+
+
+
+
+
