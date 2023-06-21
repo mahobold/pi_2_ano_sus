0918ea776a8cf3f8ae5cdf9f9699fe0dbe8df9bc
ANA BEATRIZ FERREIRA PASSOS <passos.2967@aluno.pr.senac.br>
2023-06-21 08:00:06 -0300

    atualizacao

--- cadastro_medicamento.php
+++ cadastro_medicamento.php
@@ -16,7 +16,6 @@
 
 <!DOCTYPE html>
 <html lang="pt-br">
-<<<<<<< HEAD
         <head>
             <meta charset="UTF-8">
             <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -26,20 +25,6 @@
             <title>Cadastro Medicamento</title>
         </head>
         <body>
-=======
->>>>>>> Ana
-
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
-    <link rel="stylesheet" href="projeto.css">
-    <title>Cadastro Medicamento</title>
-</head>
-<body>
-
-<<<<<<< HEAD
             <form action="cadastro.php" method="post">
                 <label>Nome do medicamento: </label>
                 <input type="text" name="nome"/><br/><br/>
@@ -56,14 +41,5 @@
         </body>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
 </html>
-=======
-    <?php
-    include("menu.php");
-    ?>
->>>>>>> Ana
 
 
-</body>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
-
-</html>
