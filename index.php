09dd433f4cfc846d8ef86625d058e4e08e2b837e
ANA BEATRIZ FERREIRA PASSOS <passos.2967@aluno.pr.senac.br>
2023-06-21 12:10:11 -0300

    atualizacao

--- index.php
+++ index.php
@@ -43,7 +43,8 @@
         <span class="visually-hidden">Next</span>
       </button>
     </div>
-  </body>
+  
+    <div class="index-container">
   <h1>ACESSE:</h1>
   <a class="navegar" href="">Unidades próximas</a>
   <br>
@@ -54,6 +55,8 @@
   <a class="navegar" href="">Info doenças</a>
   <br>
   <a class="navegar" href="">Minha conta</a>
+
+  </div>
 
 <!---- ###################################################################################################################################################################### -->
   <div class="faixaazul"></div> <!---- Colocar altura e cor azul -->
@@ -74,7 +77,8 @@
     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-telephone-fill" viewBox="0 0 16 16">
       <path fill-rule="evenodd" d="M1.885.511a1.745 1.745 0 0 1 2.61.163L6.29 2.98c.329.423.445.974.315 1.494l-.547 2.19a.678.678 0 0 0 .178.643l2.457 2.457a.678.678 0 0 0 .644.178l2.189-.547a1.745 1.745 0 0 1 1.494.315l2.306 1.794c.829.645.905 1.87.163 2.611l-1.034 1.034c-.74.74-1.846 1.065-2.877.702a18.634 18.634 0 0 1-7.01-4.42 18.634 18.634 0 0 1-4.42-7.009c-.362-1.03-.037-2.137.703-2.877L1.885.511z"/>
     </svg>
-
+    
+    </body>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/js/bootstrap.bundle.min.js" integrity="sha384-qKXV1j0HvMUeCBQ+QVp7JcfGl760yU08IQ+GpUo5hlbpg51QRiuqHAJz8+BrxE/N" crossorigin="anonymous"></script>
 
 </html>
