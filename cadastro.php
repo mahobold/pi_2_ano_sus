9c94feb99a80d8e724d85da4824cb198b1c543cc
LETICIA FERNANDA PEREIRA <leticia.6978@aluno.pr.senac.br>
2023-06-21 11:42:19 -0300

    Scriptle

--- cadastro.php
+++ cadastro.php
@@ -17,5 +17,21 @@
     </form>
     <p><a href="login.php">Faça login</a></p>
   </div>
+  <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11.7.12/dist/sweetalert2.all.min.js"></script>
+        <script>
+            function alert(){
+                    Swal.fire(
+                        {
+                            text: 'Sucesso ao cadastrar.',
+                            icon: 'success',
+                            title: 'Sucesso',
+                           
+                        }
+)
+                    }
+                    alert("Você será redirecionado");
+                           
+
+        </script>
 </body>
 </html>
