adccca59a3e2c99d277b863e967230df30247a35
ANA BEATRIZ FERREIRA PASSOS <passos.2967@aluno.pr.senac.br>
2023-08-09 12:10:00 -0300

    att

--- farmacia.php
+++ farmacia.php
@@ -168,6 +168,10 @@
       </div>
     </div>
   </div>
+ 
+  <?php
+  include("rodape.php");
+  ?>
 
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick.min.js"></script>
