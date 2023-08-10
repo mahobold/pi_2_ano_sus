1f00963a4242c9e921cfbbd159e23003c634a01d
LUIZ EDUARDO ROSAESKI BONFIM <bonfim.2979@aluno.pr.senac.br>
2023-08-10 09:04:18 -0300

    ATUALIZACAOLUIZ

--- postao.php
+++ postao.php
@@ -69,7 +69,7 @@
     <div class="row">
       <!-- ##################################################################################  -->
       <div class="card" style="width: 18rem;"> <!-- Card Medico plantão do dia -->
-        <img src="imagens/Alex_Gonçalves3.jpg" class="card-img-top" alt="...">
+        <img src="img/Alex_Gonçalves3.jpg" class="card-img-top" alt="...">
         <div class="card-body">
           <h5 class="titulo">Medico plantão do dia.</h5>
           <p class="card-text">
@@ -153,6 +153,11 @@
             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-telephone-fill" viewBox="0 0 16 16">
               <path fill-rule="evenodd" d="M1.885.511a1.745 1.745 0 0 1 2.61.163L6.29 2.98c.329.423.445.974.315 1.494l-.547 2.19a.678.678 0 0 0 .178.643l2.457 2.457a.678.678 0 0 0 .644.178l2.189-.547a1.745 1.745 0 0 1 1.494.315l2.306 1.794c.829.645.905 1.87.163 2.611l-1.034 1.034c-.74.74-1.846 1.065-2.877.702a18.634 18.634 0 0 1-7.01-4.42 18.634 18.634 0 0 1-4.42-7.009c-.362-1.03-.037-2.137.703-2.877L1.885.511z" />
             </svg>(43) 9 3472-7113
+          </p>
+          <p class="card-text">
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-geo-alt-fill" viewBox="0 0 16 16">
+              <path d="M8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10zm0-7a3 3 0 1 1 0-6 3 3 0 0 1 0 6z" />
+            </svg> Rua ####.
           </p>
           <a href="https://goo.gl/maps/ctjNLhTUHtY3PDTK7" class="btn btn-primary">Endereço aqui.</a>
         </div>
@@ -268,7 +273,7 @@
 
           <!-- ################################ Card Ortopedista plantão do dia ##################################################  --> 
       <div class="card" style="width: 18rem;"> 
-        <img src="imagens/medico4.jpg" class="card-img-top" alt="...">
+        <img src="img/esveraldo.jpg" class="card-img-top" alt="...">
         <div class="card-body">
           <h5 class="card-title">Ortopedista plantão do dia.</h5>
            <p class="card-text">
@@ -369,7 +374,7 @@
       <div class="row">
       <!-- ##################################################################################  -->
       <div class="card" style="width: 18rem;"> <!-- Card Medico plantão do dia -->
-        <img src="imagens/luiz_passos3.jpg" class="card-img-top" alt="...">
+        <img src="img/luiz_passos3.jpg" class="card-img-top" alt="...">
         <div class="card-body">
           <h5 class="card-title">Medico plantão do dia.</h5>
           <p class="card-text">
@@ -393,7 +398,7 @@
       
       <!-- ################################ Card Pediatra plantão do dia ##################################################  --> 
       <div class="card" style="width: 18rem;"> 
-        <img src="imagens/medico6.jpg" class="card-img-top" alt="...">
+        <img src="img/esveraldo.jpg" class="card-img-top" alt="...">
         <div class="card-body">
           <h5 class="card-title">Pediatra plantão do dia.</h5>
           <p class="card-text">
@@ -420,7 +425,7 @@
 
           <!-- ################################ Card Ortopedista plantão do dia ##################################################  --> 
       <div class="card" style="width: 18rem;"> 
-        <img src="imagens/Maria_Araújo.jpg" class="card-img-top" alt="...">
+        <img src="img/Maria_Araújo.jpg" class="card-img-top" alt="...">
         <div class="card-body">
           <h5 class="card-title">Ortopedista plantão do dia.</h5>
           <p class="card-text">
@@ -453,6 +458,11 @@
             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-telephone-fill" viewBox="0 0 16 16">
               <path fill-rule="evenodd" d="M1.885.511a1.745 1.745 0 0 1 2.61.163L6.29 2.98c.329.423.445.974.315 1.494l-.547 2.19a.678.678 0 0 0 .178.643l2.457 2.457a.678.678 0 0 0 .644.178l2.189-.547a1.745 1.745 0 0 1 1.494.315l2.306 1.794c.829.645.905 1.87.163 2.611l-1.034 1.034c-.74.74-1.846 1.065-2.877.702a18.634 18.634 0 0 1-7.01-4.42 18.634 18.634 0 0 1-4.42-7.009c-.362-1.03-.037-2.137.703-2.877L1.885.511z" />
             </svg>(43) 93472-1788
+          </p>
+          <p class="card-text">
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-geo-alt-fill" viewBox="0 0 16 16">
+              <path d="M8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10zm0-7a3 3 0 1 1 0-6 3 3 0 0 1 0 6z" />
+            </svg>Rua ####.
           </p>
           <a href="https://goo.gl/maps/HzV7zsftdxPyaEX28" class="btn btn-primary">Endereço Aqui.</a>
         </div>
