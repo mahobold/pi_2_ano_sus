1191f87b4e84d3abb9b8ca4dfc5b98fc6c46bef1
MARIA VITORIA MAGALHAES HOBOLD <hobold.7994@aluno.pr.senac.br>
2023-08-11 08:39:54 -0300

    posto

--- postao.php
+++ postao.php
@@ -16,10 +16,11 @@
 <head>
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <link rel="icon" href="img/logo2.png">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-aFq/bzH65dt+w6FI2ooMVUpc+21e0SRygnTpmBvdBgSdnuTN7QbdgL+OapgHtvPp" crossorigin="anonymous">
   <link rel="stylesheet" href="projeto.css">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Luiz dela</title>
+  <title>Postos</title>
 </head>
 
 <body>
@@ -100,7 +101,7 @@
               <path fill-rule="evenodd" d="M1.885.511a1.745 1.745 0 0 1 2.61.163L6.29 2.98c.329.423.445.974.315 1.494l-.547 2.19a.678.678 0 0 0 .178.643l2.457 2.457a.678.678 0 0 0 .644.178l2.189-.547a1.745 1.745 0 0 1 1.494.315l2.306 1.794c.829.645.905 1.87.163 2.611l-1.034 1.034c-.74.74-1.846 1.065-2.877.702a18.634 18.634 0 0 1-7.01-4.42 18.634 18.634 0 0 1-4.42-7.009c-.362-1.03-.037-2.137.703-2.877L1.885.511z" />
             </svg>(43) 9 9999-9999
           </p>
-          <a href="#" class="btn btn-primary">Acesse o currículo aqui</a>
+          <a href="pdf/Alex_gONÇALVES.pdf" class="btn btn-primary">Ver formação</a>
         </div>
       </div> <!-- FIM Card Medico plantão do dia -->
       
@@ -124,7 +125,7 @@
               <path fill-rule="evenodd" d="M1.885.511a1.745 1.745 0 0 1 2.61.163L6.29 2.98c.329.423.445.974.315 1.494l-.547 2.19a.678.678 0 0 0 .178.643l2.457 2.457a.678.678 0 0 0 .644.178l2.189-.547a1.745 1.745 0 0 1 1.494.315l2.306 1.794c.829.645.905 1.87.163 2.611l-1.034 1.034c-.74.74-1.846 1.065-2.877.702a18.634 18.634 0 0 1-7.01-4.42 18.634 18.634 0 0 1-4.42-7.009c-.362-1.03-.037-2.137.703-2.877L1.885.511z" />
             </svg>(43) 9 9999-9999
           </p>
-          <a href="#" class="btn btn-primary">Acesse o currículo aqui.</a>
+          <a href="pdf/curriculo3.pdf" class="btn btn-primary">Ver formação</a>
         </div>
 
       </div>
@@ -151,7 +152,7 @@
               <path fill-rule="evenodd" d="M1.885.511a1.745 1.745 0 0 1 2.61.163L6.29 2.98c.329.423.445.974.315 1.494l-.547 2.19a.678.678 0 0 0 .178.643l2.457 2.457a.678.678 0 0 0 .644.178l2.189-.547a1.745 1.745 0 0 1 1.494.315l2.306 1.794c.829.645.905 1.87.163 2.611l-1.034 1.034c-.74.74-1.846 1.065-2.877.702a18.634 18.634 0 0 1-7.01-4.42 18.634 18.634 0 0 1-4.42-7.009c-.362-1.03-.037-2.137.703-2.877L1.885.511z" />
             </svg>(22) 2 2222-2222
           </p>
-          <a href="#" class="btn btn-primary">Acesse o currículo aqui.</a>
+          <a href="pdf/curriculo2.pdf" class="btn btn-primary">Ver formação</a>
         </div>
 
       </div>
@@ -253,7 +254,7 @@
               <path fill-rule="evenodd" d="M1.885.511a1.745 1.745 0 0 1 2.61.163L6.29 2.98c.329.423.445.974.315 1.494l-.547 2.19a.678.678 0 0 0 .178.643l2.457 2.457a.678.678 0 0 0 .644.178l2.189-.547a1.745 1.745 0 0 1 1.494.315l2.306 1.794c.829.645.905 1.87.163 2.611l-1.034 1.034c-.74.74-1.846 1.065-2.877.702a18.634 18.634 0 0 1-7.01-4.42 18.634 18.634 0 0 1-4.42-7.009c-.362-1.03-.037-2.137.703-2.877L1.885.511z" />
             </svg>(43) 9 9999-9999
           </p>
-          <a href="#" class="btn btn-primary">Acesse o currículo aqui.</a>
+          <a href="pdf/curriculo7.pdf" class="btn btn-primary">Ver formação</a>
         </div>
       </div> <!-- FIM Card Medico plantão do dia -->
       
@@ -277,7 +278,7 @@
               <path fill-rule="evenodd" d="M1.885.511a1.745 1.745 0 0 1 2.61.163L6.29 2.98c.329.423.445.974.315 1.494l-.547 2.19a.678.678 0 0 0 .178.643l2.457 2.457a.678.678 0 0 0 .644.178l2.189-.547a1.745 1.745 0 0 1 1.494.315l2.306 1.794c.829.645.905 1.87.163 2.611l-1.034 1.034c-.74.74-1.846 1.065-2.877.702a18.634 18.634 0 0 1-7.01-4.42 18.634 18.634 0 0 1-4.42-7.009c-.362-1.03-.037-2.137.703-2.877L1.885.511z" />
             </svg>(43) 9 9999-9999
           </p>
-          <a href="#" class="btn btn-primary">Acesse o currículo aqui.</a>
+          <a href="pdf/Currículo de administração azul masculino.pdf" class="btn btn-primary">Ver formação</a>
         </div>
 
       </div>
@@ -304,7 +305,7 @@
               <path fill-rule="evenodd" d="M1.885.511a1.745 1.745 0 0 1 2.61.163L6.29 2.98c.329.423.445.974.315 1.494l-.547 2.19a.678.678 0 0 0 .178.643l2.457 2.457a.678.678 0 0 0 .644.178l2.189-.547a1.745 1.745 0 0 1 1.494.315l2.306 1.794c.829.645.905 1.87.163 2.611l-1.034 1.034c-.74.74-1.846 1.065-2.877.702a18.634 18.634 0 0 1-7.01-4.42 18.634 18.634 0 0 1-4.42-7.009c-.362-1.03-.037-2.137.703-2.877L1.885.511z" />
             </svg>(22) 2 2222-2222
           </p>
-          <a href="#" class="btn btn-primary">Acesse o currilo aqui</a>
+          <a href="pdf/curriculo4.pdf" class="btn btn-primary">Ver formação</a>
         </div>
 
       </div>
@@ -405,7 +406,7 @@
               <path fill-rule="evenodd" d="M1.885.511a1.745 1.745 0 0 1 2.61.163L6.29 2.98c.329.423.445.974.315 1.494l-.547 2.19a.678.678 0 0 0 .178.643l2.457 2.457a.678.678 0 0 0 .644.178l2.189-.547a1.745 1.745 0 0 1 1.494.315l2.306 1.794c.829.645.905 1.87.163 2.611l-1.034 1.034c-.74.74-1.846 1.065-2.877.702a18.634 18.634 0 0 1-7.01-4.42 18.634 18.634 0 0 1-4.42-7.009c-.362-1.03-.037-2.137.703-2.877L1.885.511z" />
             </svg>(43) 9 9999-9999
           </p>
-          <a href="#" class="btn btn-primary">Acesse o currículo aqui.</a>
+          <a href="pdf/Luiz_passos.pdf" class="btn btn-primary">Ver formação</a>
         </div>
       </div> <!-- FIM Card Medico plantão do dia -->
       
@@ -429,7 +430,7 @@
               <path fill-rule="evenodd" d="M1.885.511a1.745 1.745 0 0 1 2.61.163L6.29 2.98c.329.423.445.974.315 1.494l-.547 2.19a.678.678 0 0 0 .178.643l2.457 2.457a.678.678 0 0 0 .644.178l2.189-.547a1.745 1.745 0 0 1 1.494.315l2.306 1.794c.829.645.905 1.87.163 2.611l-1.034 1.034c-.74.74-1.846 1.065-2.877.702a18.634 18.634 0 0 1-7.01-4.42 18.634 18.634 0 0 1-4.42-7.009c-.362-1.03-.037-2.137.703-2.877L1.885.511z" />
             </svg>(43) 9 9999-9999
           </p>
-          <a href="#" class="btn btn-primary">Acesse o currículo aqui.</a>
+          <a href="pdf/curriculo6.pdf" class="btn btn-primary">Ver formação</a>
         </div>
 
       </div>
@@ -456,7 +457,7 @@
               <path fill-rule="evenodd" d="M1.885.511a1.745 1.745 0 0 1 2.61.163L6.29 2.98c.329.423.445.974.315 1.494l-.547 2.19a.678.678 0 0 0 .178.643l2.457 2.457a.678.678 0 0 0 .644.178l2.189-.547a1.745 1.745 0 0 1 1.494.315l2.306 1.794c.829.645.905 1.87.163 2.611l-1.034 1.034c-.74.74-1.846 1.065-2.877.702a18.634 18.634 0 0 1-7.01-4.42 18.634 18.634 0 0 1-4.42-7.009c-.362-1.03-.037-2.137.703-2.877L1.885.511z" />
             </svg>(22) 2 2222-2222
           </p>
-          <a href="#" class="btn btn-primary">Acesse o currículo aqui.</a>
+          <a href="pdf/Maria_Araújo.pdf" class="btn btn-primary">Ver formação</a>
         </div>
 
       </div>
