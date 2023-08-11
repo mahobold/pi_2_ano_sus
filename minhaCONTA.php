4ed2e4276e1c522979f8ff728eb5c57cdb6f6976
LETICIA FERNANDA PEREIRA <leticia.6978@aluno.pr.senac.br>
2023-08-11 08:17:12 -0300

    ALTERACAOLE

--- minhaCONTA.php
+++ minhaCONTA.php
@@ -9,14 +9,15 @@
 </head>
 <body>
     <h1>Olá ...</h1>
-    <h1>Minhas informações</h1>
+    <h1>Minhas informações <a href="" ></a></h1>
     <h1>Nome</h1>
+    <h1>Nome de usuário</h1>
     <h1>Telefone</h1>
     <h1>Endereço</h1>
     <h1>CPF</h1>
     <h1>Email</h1>
     <h1>Número do Cartão Sus</h1>
-    <h1>Senha</h1>
+    <h1>Marcar Consulta <a href="consulta.php">Marque sua Consulta</a></h1>
     
 </body>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
