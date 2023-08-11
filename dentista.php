44cfb6706ba8bcd53fd0924a41d4b39fee5764c9
ANA BEATRIZ FERREIRA PASSOS <passos.2967@aluno.pr.senac.br>
2023-08-11 07:56:54 -0300

    att

--- dentista.php
+++ dentista.php
@@ -12,33 +12,35 @@
 <?php
     include("menu.php");
     ?>
-    <br>
-    <br>
-    <br>
+
+    <div class="consultar">
   <h1>  Agende sua consulta com o dentista aqui: </h1>
-    <br>
-    <br>
-    <img src="img\Maria_Araújo.jpg" alt="">
-    <br>
-    <br>
-    <br>
-    <h2>Questionário base</h2>
-  <form action="" method="post">
-        <label>Nome completo:</label>
-        <input type="text" name="bt_nome" /><br /><br />
-        <label>Idade:</label>
-        <input type="text" name="bt_idade" /><br /><br />
-        <label>Quando foi a última ida ao dentista?</label>
-        <input type="text" name="bt_ida_dentista" /><br /><br />
-        <label> Veio por motivo estético ou de saúde? </label>
-        <input type="text" name="bt_estetica" /><br /><br />
-        <label>Já usou aparelho?</label>
-        <input type="text" name="bt_dor_aparelho" /><br /><br />
-        <input type="submit" class="btn btn-success" value="Enviar">
-        <input type="reset" class="btn btn-danger" value="Limpar">
-        </div>
+  </div>
+
+    <img src="img\" alt="">
+    
+    <div class="card text-bg-dark">
+  <img src="img\Maria_Araújo.jpg" class="card-img" alt="...">
+  <div class="card-img-overlay">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+    <p class="card-text"><small>Last updated 3 mins ago</small></p>
+  </div>
+</div>
+
+    <div class="dentista-container">
+    <h2 class="add">Questionário base</h2>
+    <form action="" method="post">
+      <input type="text" name="nome" placeholder="Nome Completo" required>
+      <input type="text" name="idade" placeholder="Idade" required>
+      <input type="text" name="tontura" placeholder="Quando foi a última ida ao dentista?" required>
+      <input type="text" name="cabeca" placeholder="Veio por motivo estético ou de saúde?" required>
+      <input type="text" name="desmaio" placeholder="Já usou aparelho?" required>
+      <input type="submit" value="Cadastrar" onclick="return validateFields()">
     </form>
-    </form>
+    <p><a href="consulta.php">Voltar</a></p>
+  </div>
+  
  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
 </body>
 </html>
