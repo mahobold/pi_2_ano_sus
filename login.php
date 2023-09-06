a613a20b68060484b073ad05cd4fb422458ec8c8
Dieimes Nunes de Souza <dieimes.souza@docente.pr.senac.br>
2023-09-06 11:37:56 -0300

    dlfnldr

--- login.php
+++ login.php
@@ -46,7 +46,7 @@
 <head>
 <link rel="icon" href="img/logo2.png">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
-    <link rel="stylesheet" href="css/projeto.css">
+    <link rel="stylesheet" href="css/projetos.css">
 
     <title>Página de Login</title>
 </head>
