398cb3e494028410d4e1e049c86bb1452a6581cc
Dieimes Nunes de Souza <dieimes.souza@docente.pr.senac.br>
2023-10-05 08:43:39 -0300

    Atualização

--- cadastroo.php
+++ cadastroo.php
@@ -9,13 +9,13 @@
         $cpf = $_POST['cpf'];
         $datanasc = $_POST['datanasc'];
         $email = $_POST['email'];
-        $caminho = "Imagens/foto_perfil.jpg";
+        
         $numerocartaosus = $_POST['numerocartaosus'];
         $senha = password_hash ($_POST['senha'], PASSWORD_DEFAULT);
         $camimg = "ftperfil/foto_teste.png";
 
         $mysqli->query("INSERT INTO pessoas (nome, sobrenome, telefone, endereco, cpf, datanasc, email, numerocartaosus, senha, camimg)
-        values ('$nome', '$sobrenome', '$telefone', '$endereco', '$cpf', '$datanasc', '$email', '$numerocartaosus', '$senha' '$camimg')")
+        values ('$nome', '$sobrenome', '$telefone', '$endereco', '$cpf', '$datanasc', '$email', '$numerocartaosus', '$senha' , '$camimg')")
         or die($mysqli->error);
 
     }
