9bcfbeced39735e4797b217e8be7ed5d3c4e460d
MARCELA LUIZA RAMOS REIS <marcela.6966@aluno.pr.senac.br>
2023-10-16 08:45:49 -0300

    mm

--- clinico_geral.php
+++ clinico_geral.php
@@ -20,6 +20,7 @@
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
+<link rel="icon" href="img/logo2.png">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="icon" href="img/logo2.png">
