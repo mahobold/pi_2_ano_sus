647003f2f550bf709e2e82e349f8214c215c7c57
Dieimes Nunes de Souza <dieimes.souza@docente.pr.senac.br>
2023-10-17 11:58:32 -0300

    sdsadasd

--- cadastro_especialidade.php
+++ cadastro_especialidade.php
@@ -1,18 +1,18 @@
 <?php
-    include("conexao.php");
+include("conexao.php");
 
-    if (isset($_POST['especialidade'])) {
-        $especialidade = $_POST['especialidade'];
-        $mysqli->query("INSERT INTO cadastro_especialidade (especialidade)
+if (isset($_POST['especialidade'])) {
+    $especialidade = $_POST['especialidade'];
+    $mysqli->query("INSERT INTO cadastro_especialidade (especialidade)
         values ('$especialidade')")
         or die($mysqli->error);
-
-    }
+}
 
 
 ?>
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -21,12 +21,19 @@
     <link rel="stylesheet" href="css/projeto.css">
     <title>Cadastro - Especialidade</title>
 </head>
+
 <body>
-<form action="" method="post">
-      <input type="text" name="especialidade" placeholder="Especialidade" required>
-      <input type="submit" value="Cadastrar" onclick="return validateFields()">
+    <?php
+        include("menu.php");
+    ?>
+
+    
+    <form action="" method="post">
+        <input type="text" name="especialidade" placeholder="Especialidade" required>
+        <input type="submit" value="Cadastrar" onclick="return validateFields()">
 
     </form>
 </body>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
+
 </html>
