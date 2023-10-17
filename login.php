0ba9a4027d526e39bf6130a265ced1ced671c901
MATHEUS WAGATUMA SPADRIZANI <spadrizani.8935@aluno.pr.senac.br>
2023-10-17 11:57:02 -0300

    tta

--- login.php
+++ login.php
@@ -68,7 +68,9 @@
     
 
      
-
+    <?php
+  include("rodape2.php");
+  ?>
 </body>
 <script src="cpf.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
