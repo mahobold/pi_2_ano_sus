6a5c890baf13af6899c3f1f661f69548d7a3b386
MATHEUS WAGATUMA SPADRIZANI <spadrizani.8935@aluno.pr.senac.br>
2023-10-18 08:59:58 -0300

    tentandonov

--- postao.php
+++ postao.php
@@ -480,7 +480,9 @@
     <div id="responsivel" class="bloco2">
       </div>
 
-
+    <?php
+  include("rodape2.php");
+  ?>      
 </body>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/js/bootstrap.bundle.min.js" integrity="sha384-qKXV1j0HvMUeCBQ+QVp7JcfGl760yU08IQ+GpUo5hlbpg51QRiuqHAJz8+BrxE/N" crossorigin="anonymous"></script>
 
