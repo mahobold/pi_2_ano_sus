9dfd34a76a10fb071342b6910d11199bc5574901
Dieimes Nunes de Souza <dieimes.souza@docente.pr.senac.br>
2023-10-19 11:10:15 -0300

    msg

--- info.php
+++ info.php
@@ -150,18 +150,14 @@
 
         </div>
       </div>
-
-       
+     
       <div class="card" style="width: 18rem;">
         <img src="img/covidd1.jpg" class="card-img-top" alt="...">
         <div class="card-body">
           <h5 class="card-title"></h5>
           <p class="card-text">Evite contato próximo</p>
-
         </div>
       </div>
-
-
       <div class="card" style="width: 18rem;">
         <img src="img/covid3.jfif" class="card-img-top" alt="...">
         <div class="card-body">
