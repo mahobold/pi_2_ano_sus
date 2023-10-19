e78c4f4e0a6bf5de20a842cd051716a42d92bbfd
Dieimes Nunes de Souza <dieimes.souza@docente.pr.senac.br>
2023-10-19 11:52:11 -0300

    Leticia

---
+++ postao2.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+    <html lang="pt-br">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-aFq/bzH65dt+w6FI2ooMVUpc+21e0SRygnTpmBvdBgSdnuTN7QbdgL+OapgHtvPp" crossorigin="anonymous">
+        <link rel="stylesheet" href="css/projeto.css">
+    </head>
+    <body>
+        <?php
+            include("menu.php");
+        ?>
+    </body>
+</html>
