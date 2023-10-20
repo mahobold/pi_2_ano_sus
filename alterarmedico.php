12402a90cc0c558413c9b120e02500b4396ddb22
ANA BEATRIZ FERREIRA PASSOS <passos.2967@aluno.pr.senac.br>
2023-10-20 08:21:00 -0300

    att

--- alterarmedico.php
+++ alterarmedico.php
@@ -61,7 +61,7 @@ if (isset($_POST['nomemedico'])) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="icon" href="img/logo2.png">
     <title>Alterar - Médico</title>
-    <link rel="stylesheet" href="css/projeto.css">
+    <link rel="stylesheet" href="css/dieimes.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
 
 </head>
