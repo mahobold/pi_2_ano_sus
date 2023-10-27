472161ac4ea6dd9097a658315eb0ab6624b27ed3
MATHEUS WAGATUMA SPADRIZANI <spadrizani.8935@aluno.pr.senac.br>
2023-10-27 08:35:25 -0300

    issso

--- static/rodape.php
+++ static/rodape.php
@@ -5,18 +5,23 @@
   <footer class="py-3 my-4">
     <ul class="nav justify-content-center border-bottom pb-3 mb-3">
 
-        <p>DESENVOLVIDO POR:</p>
+        <li>DESENVOLVIDO POR:</li>
 
-      <li class="nav-item"><a href="https://www.instagram.com/mahobold/" class="nav-link px-2 text-body-secondary">Maria</a></li>
-      <li class="nav-item"><a href="https://www.instagram.com/itsmeanah_/" class="nav-link px-2 text-body-secondary">Ana</a></li>
-      <li class="nav-item"><a href="https://www.instagram.com/leepereiraf/" class="nav-link px-2 text-body-secondary">Letícia</a></li>
-      <li class="nav-item"><a href="https://www.instagram.com/marcelaluizareis/" class="nav-link px-2 text-body-secondary">Marcela</a></li>
-      <li class="nav-item"><a href="https://www.instagram.com/bonfim_xt/" class="nav-link px-2 text-body-secondary">Luiz</a></li>
-      <li class="nav-item"><a href="https://www.instagram.com/w_matheuso/" class="nav-link px-2 text-body-secondary">Matheus</a></li>
+      <li href="https://www.instagram.com/mahobold/" class="nav-link px-2 text-body-secondary">Maria</li>
       <br>
-      <p>ORIENTADO POR:</p>
+      <li href="https://www.instagram.com/itsmeanah_/" class="nav-link px-2 text-body-secondary">Ana</li>
       <br>
-      <li class="nav-item"><a href="https://www.instagram.com/dieimes.nunes/" class="nav-link px-2 text-body-secondary">Dieimes</a></li>
+      <li href="https://www.instagram.com/leepereiraf/" class="nav-link px-2 text-body-secondary">Letícia</li>
+      <br>
+      <li href="https://www.instagram.com/marcelaluizareis/" class="nav-link px-2 text-body-secondary">Marcela</li>
+      <br>
+      <li href="https://www.instagram.com/bonfim_xt/" class="nav-link px-2 text-body-secondary">Luiz</li>
+      <br>
+      <li href="https://www.instagram.com/w_matheuso/" class="nav-link px-2 text-body-secondary">Matheus</li>
+      <br>
+      <li>ORIENTADO POR:</li>
+      <br>
+      <li href="https://www.instagram.com/dieimes.nunes/" class="nav-link px-2 text-body-secondary">Dieimes</li>
 
 
     </ul>
