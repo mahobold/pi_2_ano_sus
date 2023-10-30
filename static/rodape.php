02080f36bd2ccd401e9ef815da71dd7a6a84433c
MATHEUS WAGATUMA SPADRIZANI <spadrizani.8935@aluno.pr.senac.br>
2023-10-30 08:43:33 -0300

    oi

--- static/rodape.php
+++ static/rodape.php
@@ -5,27 +5,32 @@
   <footer class="py-3 my-4">
     <ul class="nav justify-content-center border-bottom pb-3 mb-3">
 
-        <li>DESENVOLVIDO POR:</li>
+      <div class="container d-flex justify-content-center">
+      <li>DESENVOLVIDO POR:</li>
+      </div>
 
-      <li href="https://www.instagram.com/mahobold/" class="nav-link px-2 text-body-secondary">Maria</li>
-      <br>
-      <li href="https://www.instagram.com/itsmeanah_/" class="nav-link px-2 text-body-secondary">Ana</li>
-      <br>
-      <li href="https://www.instagram.com/leepereiraf/" class="nav-link px-2 text-body-secondary">Letícia</li>
-      <br>
-      <li href="https://www.instagram.com/marcelaluizareis/" class="nav-link px-2 text-body-secondary">Marcela</li>
-      <br>
-      <li href="https://www.instagram.com/bonfim_xt/" class="nav-link px-2 text-body-secondary">Luiz</li>
-      <br>
-      <li href="https://www.instagram.com/w_matheuso/" class="nav-link px-2 text-body-secondary">Matheus</li>
-      <br>
+      <li class="nav-item"><a href="https://www.instagram.com/mahobold/" class="nav-link px-2 text-body-secondary">Maria</a></li>
+
+      <li class="nav-item"><a href="https://www.instagram.com/itsmeanah_/" class="nav-link px-2 text-body-secondary">Ana</a></li>
+
+      <li class="nav-item"><a href="https://www.instagram.com/leepereiraf/" class="nav-link px-2 text-body-secondary">Letícia</a></li>
+
+      <li class="nav-item"><a href="https://www.instagram.com/marcelaluizareis/" class="nav-link px-2 text-body-secondary">Marcela</a></li>
+
+      <li class="nav-item"><a href="https://www.instagram.com/bonfim_xt/" class="nav-link px-2 text-body-secondary">Luiz</a></li>
+
+      <li class="nav-item"><a href="https://www.instagram.com/w_matheuso/" class="nav-link px-2 text-body-secondary">Matheus</a></li>
+
+      <div class="container d-flex justify-content-center">
       <li>ORIENTADO POR:</li>
-      <br>
-      <li href="https://www.instagram.com/dieimes.nunes/" class="nav-link px-2 text-body-secondary">Dieimes</li>
+      </div>
 
+      <div class="container d-flex justify-content-center">
+      <li class="nav-"><a href="https://www.instagram.com/dieimes.nunes/" class="nav-link px-2 text-body-secondary">Dieimes</a></li>
+      </div>
 
     </ul>
-    <a href="https://www.senac.br/" "target=_blank"><img src="img/333.jpg"></a>
+    <a href="https://www.senac.br/" target="_blank"> <img src="img/333.jpg"></a>
   </footer>
 </div>
 
