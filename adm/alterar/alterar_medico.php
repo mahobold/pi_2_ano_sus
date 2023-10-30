807aee7c6e9cccf4181761116151a7e973cf4120
Dieimes Nunes de Souza <dieimes.souza@docente.pr.senac.br>
2023-10-30 11:30:46 -0300

    Atualização

--- adm/alterar/alterar_medico.php
+++ adm/alterar/alterar_medico.php
@@ -1,16 +1,10 @@
 <?php
 include("../conexao.php");
-/* require("autenticacao.php"); Falta fazer */
 
 
+// Se os dados do médico forem enviados via POST
 if (isset($_POST['id_medico'])) {
-
-    echo "<pre>";
-        print_r($_POST);
-    echo "</pre>";
-
-
-
+    // Recupere os dados do POST
     $id_medico = $_POST['id_medico'];
     $nomemedico = $_POST['nomemedico'];
     $idade = $_POST['idade'];
@@ -19,31 +13,34 @@ if (isset($_POST['id_medico'])) {
     $telefone = $_POST['telefone'];
     $especialidade = $_POST['especialidade'];
 
-
-
-    $sql_alterar = "UPDATE medico
-        SET nome = '$nomemedico',
-        idade = '$idade',
-        crm = '$crm',
-        endereco = '$endereco',
-        telefone = '$telefone',
-        especialidade = '$especialidade'
-        WHERE id_medico = '$id_medico'";
-
+    // Usando prepared statements para atualizar os dados
+    $stmt = $mysqli->prepare("UPDATE medico SET nome = ?, idade = ?, crm = ?, endereco = ?, telefone = ?, especialidade = ? WHERE id_medico = ?");
+    $stmt->bind_param('sssssss', $nomemedico, $idade, $crm, $endereco, $telefone, $especialidade, $id_medico);
     
+    if ($stmt->execute()) {
+        $mensagem_sucesso =  "Dados atualizados com sucesso!";
+        
 
-
-    $mysqli_alterar = $mysqli->query($sql_alterar) or die($mysqli->error);
-
+    } else {
+        echo "Erro ao atualizar: " . $stmt->error;
+    }
    
+   
+    
+    $stmt->close();
+    
 }
 
+
+// Se o ID do médico for enviado via GET
 if (isset($_GET["id_medico"])) {
     $id_medico = $_GET["id_medico"];
-    $sql_consultar = "SELECT * FROM medico WHERE id_medico = ?";
-    $stmt = $mysqli->prepare($sql_consultar);
+
+    // Usando prepared statements para consultar os dados
+    $stmt = $mysqli->prepare("SELECT * FROM medico WHERE id_medico = ?");
     $stmt->bind_param('s', $id_medico);
     $stmt->execute();
+
     $result = $stmt->get_result();
     $consultar = $result->fetch_assoc();
 
@@ -66,6 +63,10 @@ if (isset($_GET["id_medico"])) {
     <title>Alterar - Médico</title>
     <link rel="stylesheet" href="css/projeto.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+    <!-- SweetAlert2 CSS -->
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/sweetalert2@11/dist/sweetalert2.min.css">
+
+
 
 </head>
 
@@ -142,14 +143,23 @@ if (isset($_GET["id_medico"])) {
     </div>
 </body>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
+<!-- SweetAlert2 JS -->
+<script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+
+<?php if(isset($mensagem_sucesso)): ?>
+<script>
+    Swal.fire({
+        icon: 'success',
+        title: 'Sucesso!',
+        text: '<?php echo $mensagem_sucesso; ?>',
+        confirmButtonColor: '#3085d6',
+        confirmButtonText: 'Ok'
+    }).then((result) => {
+        if (result.isConfirmed) {
+            window.location.href = "../consultar/medico.php";  // Redirecione para a página desejada após o usuário clicar em "Ok"
+        }
+    })
+</script>
+<?php endif; ?>
 
 </html>
-
-<?php
-// Se houve sucesso na atualização, exiba a mensagem
-/*
-if (isset($_GET['success'])) {
-    echo "<script>alert('Dados atualizados com sucesso!');</script>";
-}
-*/
-?>
--- adm/index.php
+++ adm/index.php
@@ -3,9 +3,14 @@
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="icon" href="../img/logo2.png">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-aFq/bzH65dt+w6FI2ooMVUpc+21e0SRygnTpmBvdBgSdnuTN7QbdgL+OapgHtvPp" crossorigin="anonymous">
+        <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css" rel="stylesheet">
+        <link rel="stylesheet" href="../css/dieimes.css">
         <title>Área Administrativa</title>
     </head>
     <body>
+        <?php include("menu.php"); ?>
         <h1>Área Adminstrativa</h1>
         <p>Professor está arrumando</p>
 
