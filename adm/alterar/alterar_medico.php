02ec143e0c7a6b57b77013423e32f404ed33dbbc
Dieimes Nunes de Souza <dieimes.souza@docente.pr.senac.br>
2023-10-30 10:34:42 -0300

    Atualização

--- adm/alterar/alterar_medico.php
+++ adm/alterar/alterar_medico.php
@@ -2,18 +2,55 @@
 include("../conexao.php");
 /* require("autenticacao.php"); Falta fazer */
 
-if (isset($_GET["id_medico"])) {
 
-    $id_cadastro = $_GET["id_medico"];
-    $sql_consultar = "SELECT * FROM cadastro_medico WHERE id_medico = '$id_cadastro'";
-    $mysqli_consultar = $mysqli->query($sql_consultar) or die($mysqli->error);
-    $consultar = $mysqli_consultar->fetch_assoc();
+if (isset($_POST['id_medico'])) {
 
-} else {
+    echo "<pre>";
+        print_r($_POST);
+    echo "</pre>";
+
+
+
+    $id_medico = $_POST['id_medico'];
+    $nomemedico = $_POST['nomemedico'];
+    $idade = $_POST['idade'];
+    $crm = $_POST['crm'];
+    $endereco = $_POST['endereco'];
+    $telefone = $_POST['telefone'];
+    $especialidade = $_POST['especialidade'];
+
+
+
+    $sql_alterar = "UPDATE medico
+        SET nome = '$nomemedico',
+        idade = '$idade',
+        crm = '$crm',
+        endereco = '$endereco',
+        telefone = '$telefone',
+        especialidade = '$especialidade'
+        WHERE id_medico = '$id_medico'";
+
     
+
+
+    $mysqli_alterar = $mysqli->query($sql_alterar) or die($mysqli->error);
+
+   
 }
 
+if (isset($_GET["id_medico"])) {
+    $id_medico = $_GET["id_medico"];
+    $sql_consultar = "SELECT * FROM medico WHERE id_medico = ?";
+    $stmt = $mysqli->prepare($sql_consultar);
+    $stmt->bind_param('s', $id_medico);
+    $stmt->execute();
+    $result = $stmt->get_result();
+    $consultar = $result->fetch_assoc();
+
+    $stmt->close();
+}
 ?>
+
 
 
 
@@ -40,16 +77,10 @@ if (isset($_GET["id_medico"])) {
         <form action="" method="post">
             <h1 class="text-center">Alterar - Médico</h1>
             <label class="form-label" for="">Nome</label>
-            <input type="hidden" name="id_medico" value="
-                <?php
-                if (isset($consultar['id_medico'])) {
-                    echo $consultar['id_medico'];
-                }
-
-                ?>">
+            <input type="hidden" name="id_medico" value="<?php if (isset($consultar['id_medico'])){echo $consultar['id_medico'];}?>">
             <input class="form-control" type="text" name="nomemedico" value="<?php
-                                                                                if (isset($consultar['nomemedico'])) {
-                                                                                    echo $consultar['nomemedico'];
+                                                                                if (isset($consultar['nome'])) {
+                                                                                    echo $consultar['nome'];
                                                                                 } else {
                                                                                     echo "Sem valor";
                                                                                 }
@@ -106,9 +137,19 @@ if (isset($_GET["id_medico"])) {
 
                                                                                 ?>">
             <input class="btn btn-success" type="submit" value="Alterar">
-            <a class="btn btn-primary" href="../index.php">Voltar</a>
+            <a class="btn btn-primary" href="../consultar/medico.php">Voltar</a>
         </form>
     </div>
 </body>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
+
 </html>
+
+<?php
+// Se houve sucesso na atualização, exiba a mensagem
+/*
+if (isset($_GET['success'])) {
+    echo "<script>alert('Dados atualizados com sucesso!');</script>";
+}
+*/
+?>
--- adm/consultar/medico.php
+++ adm/consultar/medico.php
@@ -1,7 +1,7 @@
 <?php
     include ("../conexao.php");
 
-    $consultar_banco = "SELECT * FROM cadastro_medico";
+    $consultar_banco = "SELECT * FROM medico";
     $retorno_consulta = $mysqli->query( $consultar_banco) or die($mysqli->error);
     $qntd= $retorno_consulta->num_rows; //retornar quantidade de linhas
 
@@ -44,7 +44,7 @@
 
                 <tr>
                    <td><?php echo $medico['id_medico']; ?></td>
-                   <td><?php echo $medico['nomemedico']; ?></td>
+                   <td><?php echo $medico['nome']; ?></td>
                    <td><?php echo $medico['idade']; ?></td>
                    <td><?php echo $medico['crm']; ?></td>
                    <td><?php echo $medico['endereco']; ?></td>
