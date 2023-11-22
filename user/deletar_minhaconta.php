2cb7a03e4812099858cb294995d0b6092fe16bc5
LETICIA FERNANDA PEREIRA <leticia.6978@aluno.pr.senac.br>
2023-11-22 09:49:53 -0300

    ATUALIZAR

--- user/deletar_minhaconta.php
+++ user/deletar_minhaconta.php
@@ -17,9 +17,14 @@
             
            // var_dump($mysqli);
         }
+
+       
+
     }else{
         echo "Código de consulta indisponível";
     }
+
+    
 
   
 ?>
@@ -34,6 +39,7 @@
         <link rel="icon" href="../img/logo2.png">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
         <link rel="stylesheet" href="../css/projeto.css">
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/sweetalert2@11/dist/sweetalert2.min.css">
         <title>Deletar Conta</title>
     </head>
 
@@ -53,6 +59,8 @@
         </div>
     </body>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
+   
+   
 </html>
    
    
