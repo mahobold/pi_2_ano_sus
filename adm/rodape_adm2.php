cf15346010448f5e7cc0b2f2d9e2e40ec67728e2
MATHEUS WAGATUMA SPADRIZANI <spadrizani.8935@aluno.pr.senac.br>
2023-11-24 07:12:21 -0300

    oi

---
+++ adm/rodape_adm2.php
@@ -0,0 +1,42 @@
+<div class="container">
+  <footer class="py-3 my-4">
+    <ul class="nav justify-content-center border-bottom pb-3 mb-3">
+
+
+    <div class="container">
+  <footer class="py-3 my-4">
+    <ul class="nav justify-content-center border-top pb-3 mb-3">
+
+
+    
+      <div class="container d-flex justify-content-center">
+      <li>DESENVOLVIDO POR:</li>
+      </div>
+
+      <li class="nav-item"><a href="https://www.instagram.com/mahobold/" target="_blank" class="nav-link px-2 text-body-secondary">Maria</a></li>
+
+      <li class="nav-item"><a href="https://www.instagram.com/itsmeanah_/" target="_blank" class="nav-link px-2 text-body-secondary">Ana</a></li>
+
+      <li class="nav-item"><a href="https://www.instagram.com/leepereiraf/" target="_blank" class="nav-link px-2 text-body-secondary">Letícia</a></li>
+
+      <li class="nav-item"><a href="https://www.instagram.com/bonfim_xt/" target="_blank" class="nav-link px-2 text-body-secondary">Luiz</a></li>
+
+      <li class="nav-item"><a href="https://www.instagram.com/marcelaluizareis/" target="_blank" class="nav-link px-2 text-body-secondary">Marcela</a></li>
+
+      <li class="nav-item"><a href="https://www.instagram.com/w_matheuso/" target="_blank" class="nav-link px-2 text-body-secondary">Matheus</a></li>
+
+      <div class="container d-flex justify-content-center">
+      <li>ORIENTADO POR:</li>
+      </div>
+
+      <div class="container d-flex justify-content-center">
+      <li class="nav-"><a href="https://www.instagram.com/dieimes.nunes/" target="_blank" class="nav-link px-2 text-body-secondary">Dieimes Nunes de Souza</a></li>
+      </div>
+
+    </ul>
+    <div class="container d-flex justify-content-center">
+    <a href="https://www.senac.br/" target="_blank"> <img src="../img/333.jpg"></a>
+    </div>
+  </footer>
+</div>
+
